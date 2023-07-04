917be735999b20971e8c75539db062faaf89fd9d
ooherin <555ohr@naver.com>
2023-07-04 23:51:16 +0900

    feat: 타입스크립트 9

--- typescript/9-advanced/9-9-record.ts
+++ typescript/9-advanced/9-9-record.ts
@@ -1,17 +1,3 @@
-// type PageInfo = {
-//   title: string;
-// };
-// type Page = 'home' | 'about' | 'contact';
-
-// const nav: Record<Page, PageInfo> = {
-//   home: { title: 'Home' },
-//   about: { title: 'About' },
-//   contact: { title: 'Contact' },
-// };
-
-// type Product = 'cat' | 'dog';
-// type NewProduct = Capitalize<Product>; // 'Cat' | 'Dog'
-
 {
   //record는 객체처럼 두개를 연결하고 싶을 때 사용하는 타입이다.
   type PageInfo = {
@@ -25,4 +11,6 @@
     about: { title: "About" },
     contact: { title: "Contact" },
   };
+  type Product = "cat" | "dog";
+  type NewProduct = Capitalize<Product>; // 'Cat' | 'Dog'
 }
