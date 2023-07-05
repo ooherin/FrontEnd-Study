199a65991b850c538d76ec3bb5e50f78be2ad06c
ooherin <555ohr@naver.com>
2023-07-05 23:48:56 +0900

    feat: 타입스크립트9

--- typescript/types/연습.ts
+++ typescript/types/연습.ts
@@ -1,42 +1,42 @@
-// {
-//   interface Vehicle {
-//     moveForward(): void;
-//     moveBackward(): void;
-//   }
-//   class Car implements Vehicle {
-//     moveForward(): void {
-//       console.log("🚘가 전진합니다.");
-//     }
-//     moveBackward(): void {
-//       console.log("🚘가 후진합니다.");
-//     }
-//   }
-//   class MotoBike implements Vehicle {
-//     moveForward(): void {
-//       console.log("🏍️가 전진합니다.");
-//     }
-//     moveBackward(): void {
-//       console.log("🏍️가 후진합니다.");
-//     }
-//   }
-//   class Driver {
-//     drive(vehicle: Vehicle) {
-//       vehicle.moveBackward();
-//       vehicle.moveForward();
-//     }
-//   }
-//   // const car1 = new Car();
-//   // const motoBike1 = new MotoBike();
-//   const driver = new Driver();
-//   // driver.drive(car1);
-//   // driver.drive(motoBike1);
+{
+  interface Vehicle {
+    moveForward(): void;
+    moveBackward(): void;
+  }
+  class Car implements Vehicle {
+    moveForward(): void {
+      console.log("🚘가 전진합니다.");
+    }
+    moveBackward(): void {
+      console.log("🚘가 후진합니다.");
+    }
+  }
+  class MotoBike implements Vehicle {
+    moveForward(): void {
+      console.log("🏍️가 전진합니다.");
+    }
+    moveBackward(): void {
+      console.log("🏍️가 후진합니다.");
+    }
+  }
+  class Driver {
+    drive(vehicle: Vehicle) {
+      vehicle.moveBackward();
+      vehicle.moveForward();
+    }
+  }
+  // const car1 = new Car();
+  // const motoBike1 = new MotoBike();
+  const driver = new Driver();
+  // driver.drive(car1);
+  // driver.drive(motoBike1);
 
-//   const vehicles = [new MotoBike(), new Car()];
-//   vehicles.forEach((item) => {
-//     console.log("___________");
-//     driver.drive(item);
-//   });
-// }
+  const vehicles = [new MotoBike(), new Car()];
+  vehicles.forEach((item) => {
+    console.log("___________");
+    driver.drive(item);
+  });
+}
 
 function getSize<T>(arr: T[]): number {
   return arr.length;
